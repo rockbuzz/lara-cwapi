dce9004bf3fbb2977316d9390c30cecc4b1471df
Tiago Lopes <tiago.dweb@gmail.com>
2022-03-01 12:00:19 -0300

    chore: branch per optional argument

--- src/Commands/DeployCommand.php
+++ src/Commands/DeployCommand.php
@@ -7,24 +7,36 @@ use Illuminate\Console\Command;
 
 class DeployCommand extends Command
 {
-    protected $signature = 'cw:deploy';
+    protected $signature = 'cw:deploy {branch?}';
 
     protected $description = 'Deploy in app via git';
 
     public function handle()
     {
+        $branch = config('cloudways.git_branch_name');
+        $repo = config('cloudways.git_url');
+
         try {
+
             $operation = app('cloudways')->startGitPull(
                 config('cloudways.server_id'),
                 config('cloudways.app_id'),
-                config('cloudways.git_url'),
-                config('cloudways.git_branch_name'),
+                $repo,
+                $this->argument('branch') ?? $branch,
                 config('cloudways.deploy_path')
             );
-            $this->info("Deploy successfully! Operation ID: $operation");
+
+            $this->info("Deploy successfully!");
+            $this->newLine();
+            $this->table(
+                ['Operation ID', 'Repo', 'Branch'],
+                [$operation, $repo, $branch]
+            );
+
             return 0;
         } catch (Exception $e) {
             $this->error($e->getMessage());
+
             return 1;
         }
     }
