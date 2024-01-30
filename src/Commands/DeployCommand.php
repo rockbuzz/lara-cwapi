30f29eec7294883217d7f7111659754d0767f879
Tiago Lopes <tiago@rockbuzz.com.br>
2024-01-30 08:32:13 -0300

    feat: server option

--- src/Commands/DeployCommand.php
+++ src/Commands/DeployCommand.php
@@ -10,7 +10,7 @@ class DeployCommand extends Command
     protected $signature = 'cw:deploy 
         {branch? : The branch name of the repository.}
         {app? : The app ID of Cloudways.} 
-        {server? : The server ID of Cloudways.}';
+        {--server=}';
 
     protected $description = 'Deploy in app via git';
 
@@ -20,7 +20,7 @@ class DeployCommand extends Command
 
         try {
             $operation = app('cloudways')->startGitPull(
-                $server = (int)$this->argument('server') ?? config('cloudways.server_id'),
+                $server = (int)$this->option('server') ?? config('cloudways.server_id'),
                 $app = (int)$this->argument('app') ?? config('cloudways.app_id'),
                 $repo,
                 $branch = $this->argument('branch') ?? config('cloudways.git_branch_name'),
